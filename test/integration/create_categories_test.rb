d5ce62da0a696e157bb4bd4e55e42cd85b17dddc
Jimit Barot <jimitbarot31@gmail.com>
2020-04-22 19:22:56 +0530

    Created migration for categories, created actions for new and index and created views related to it. Added unit and integartion tests for categories.

---
+++ test/integration/create_categories_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
+  test "get new category form and create category" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: { name: "music" } }
+      follow_redirect!
+    end
+    assert_template 'categories/index'
+    assert_match "music", response.body
+  end
+
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " " } }
+      #follow_redirect!
+    end
+    assert_template 'categories/new'
+    assert_select 'h4.alert-heading'
+  end
+
+end
