a5acbce1dbf63d985a034f3d12ced66c2c5ffddf
Jimit Barot <jimitbarot31@gmail.com>
2020-04-23 19:06:40 +0530

    Added restrictions that only logged-in users can category pages.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update]
+  before_action :require_user
   before_action :require_admin, except: [:show, :index]
 
   def index
