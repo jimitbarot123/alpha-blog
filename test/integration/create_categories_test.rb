e9cb65b47361451b3a90e3690b07c53a8c9bd1f4
Jimit Barot <jimitbarot31@gmail.com>
2020-04-23 11:19:19 +0530

    Added restrictions to categories by allowing only admin users and added test cases for the same scenarios

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  # setup do
+  #   @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  # end
+
   test "get new category form and create category" do
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -13,15 +19,24 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match "music", response.body
   end
 
-  test "invalid category submission results in failure" do
-    get new_category_path
-    assert_template 'categories/new'
+  # test "invalid category submission results in failure" do
+  #   @user2 = User.create(username: "jimit", email: "jimit@example.com", password: "password2", admin: true)
+  #   sign_in_as(@user2, "password2")
+  #   get new_category_path
+  #   assert_template 'categories/new'
+  #   assert_no_difference 'Category.count' do
+  #     post categories_path, params: { category: { name: " " } }
+  #     #follow_redirect!
+  #   end
+  #   assert_template 'categories/new'
+  #   assert_select 'h4.alert-heading'
+  # end
+
+  test "should redirect create when admin not logged in" do
     assert_no_difference 'Category.count' do
-      post categories_path, params: { category: { name: " " } }
-      #follow_redirect!
+      post categories_path, params: { category: { name: "sports" } }
     end
-    assert_template 'categories/new'
-    assert_select 'h4.alert-heading'
+    assert_redirected_to categories_path
   end
 
 end
