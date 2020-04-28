4c940f06014f7489c5d817d31f3d424bdf22a64c
Jimit Barot <jimitbarot31@gmail.com>
2020-04-28 14:00:35 +0530

    Modified changes in existing view files to working properly and updated route for edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_user
-  before_action :require_same_user, only: [:edit, :update, :destroy]
-  before_action :require_admin, only: [:destroy]
+  # before_action :require_user
+  # before_action :require_same_user, only: [:edit, :update, :destroy]
+  # before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'user/edit'}
   root "pages#home"
   get 'about', to: "pages#about"
   resources :articles#, only: [:show,:index,:new,:create,:edit,:update]
