bc0050b73f8ce5bb06f8ce04fce40d68e7bd91dd
Jimit Barot <jimitbarot31@gmail.com>
2020-04-29 11:59:59 +0530

    Removed old user related files and moved views of registration from devise to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'user/edit'}
+  devise_for :users, path: '',
+              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'user/edit'},
+              controllers: { registrations: 'users/registrations'}
+  devise_scope :user do
+   get "users", to: "users/registrations#index"
+   get "user", to: "users/registrations#show"
+   #get "logout", to: "devise/sessions#destroy"
+  end
   root "pages#home"
   get 'about', to: "pages#about"
   resources :articles#, only: [:show,:index,:new,:create,:edit,:update]
-  get 'signup', to: 'users#new'
-  resources :users, except: [:new]
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  #get 'signup', to: 'users#new'
+  #resources :registrations, except: [:new]
+  # get 'login', to: 'sessions#new'
+  # post 'login', to: 'sessions#create'
+  # delete 'logout', to: 'sessions#destroy'
   resources :categories, except: [:destroy]
 end
