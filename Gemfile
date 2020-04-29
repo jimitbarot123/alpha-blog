7d914fb9185e7f1465004e7c8aaa3b6fb01518d5
Jimit Barot <jimitbarot31@gmail.com>
2020-04-29 13:13:00 +0530

    Added migration to add roles to users

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 gem 'devise-bootstrap-views', '~> 1.0'
 
+gem 'cancancan'
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '~> 1.4'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_183317) do
+ActiveRecord::Schema.define(version: 2020_04_29_073915) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 2020_04_27_183317) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "superadmin_role", default: false
+    t.boolean "supervisor_role", default: false
+    t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
